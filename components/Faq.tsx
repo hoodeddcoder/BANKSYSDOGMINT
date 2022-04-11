67b8dcc181202bb3ae37cb3de7a8f0727afc7871
HodedCoder <96161791+hoodeddcoder@users.noreply.github.com>
2022-04-11 11:58:24 +1000

    Update Faq.tsx

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -8,23 +8,50 @@ export default function Faq() {
           <p>
           Banksys Dog is a collection of 10200 unique CPU generated digital art minted as NFTs on the Ethereum blockchain. Banksys Dog isn’t just about art, it’s also about Web 3.0 and Blockchain technology – every token will get their unique art and samsung frame tv not forgetting exclusive access to the unheard of road map that you can check out at https://banksysdog.io
           </p>
-          <h3 className="text-2xl mb-2">We Get 20.4% Of TBC and NFTYP?</h3>
+          <h3 className="text-2xl mb-2">WE GET 39% OF TBC and NFTYP?</h3>
           <p>
-          As part of this collection every token will include 10 class A voting right shares of The Business Catalog and NFT your property which will work out to be 20.4% of each company (when it is possible legally) who are both changing the blockchain game – one for businesses and one for everyone. (the profit percentages will be airdropped.)
+         As part of this collection every token will include 10 class A voting right shares of The Business Catalog and NFT your property which will work out to be 39% of each company (when we figure it out legally) who are both changing the blockchain game – one for businesses and one for everyone. (the profit percentages will be airdropped to token holders.)
           </p>
-          <h3 className="text-2xl mb-2">Every Token Will Get Airdropped 10 Legit Banksy Prints As an NFT?</h3>
+          <h3 className="text-2xl mb-2">EVERY TOKEN WILL GET AIRDROPPED 10 LEGIT BANKSY PRINTS AS AN NFT & AN UNKNOWN NUMBER OF KEITH HARING'S WORK?</h3>
           <p>
-          We have partnered with an art gallery who have sourced 10 legit pest controlled certified Banksy prints that we have paid 1.5M for – from Girl with Balloon to Flower Thrower and 8 others which we’ll keep a secret for now. Every token will also own an equal % of the prints and receive royalties from lending and if we vote to sell. We will also buy land in Decentraland that we will use to create a Banksy art gallery.
+         We have partnered with an art gallery who have sourced 10 legit pest controlled certified Banksy prints that we have paid 1.5M for – from Girl with Balloon to Flower Thrower and 8 others which we’ll keep a secret for now. Every token will also own an equal % of the prints and receive royalties from lending and if we vote to sell. We will also buy land in Decentraland that we will use to create a Banksy art gallery. We also plan on airdropping an unknown number of Keith Harings work.
           </p>
           <h3 className="text-2xl mb-2">Who Is the Music Artist?</h3>
           <p>
-          The music artists name will be kept a secret but you will get to hear snippets of the music – it’s pretty f’ing brilliant. The reason the artists name will be kept a secret is… every token holder will have a vote on the name of the artist and the name of the album. All royalties will be airdropped to wallets of token holders.
+         The music artists name is J-Koda – Go check his music out – He’s pretty f’ing brilliant. 
+
+His album is called Beyond the Norm and you also get 1 of 10 album covers created by Koda himself. 
+
+All income will be airdropped to wallets of token holders.
           </p>
           <h3 className="text-2xl mb-2">We Will Be Giving 5 Million To Charity?</h3>
           <p>
-          1 million(USD) will go to the Keith Haring foundation and 1million(USD) is Banksy the UK street artists choice and that will be revealed after minting. The sports clubs will be decided on by voters based on locations of holders. The 2 charities that will get 1 million(USD) each will also be chosen by holders through process of elimation.
+          1 million(USD) will go to the Keith Haring foundation and 1million(USD) is Banksy the UK street artists choice and that will be revealed after minting. The sports clubs will be decided on by voters based on locations of holders. The 2 charities that will get 1 million(USD) each will also be chosen by holders through process of elimination.
+
+ALL VOTES WILL BE MADE THROUGH OUR MASTERMNIND DISCORD THAT ONLY TOKEN HOLDERS GET ACCESS TO!
 
 ALL VOTES WILL BE MADE THROUGH OUR DISCORD THAT ONLY TOKEN HOLDERS GET ACCESS TO!
+          </p>
+          <h3 className="text-2xl mb-2">DO THE AIRDROPPED NFT'S HAVE THE SAME UTILITIES AS THE BANKSYS DOG ONES?</h3>
+          <p>
+          NO. However, the “Beyond the Norm” album cover NFT’s and other NFT’s airdropped can be sold on Opensea.
+
+A goal of ours will be to pay each non-fungible token millions in crypto and give them lots of exciting gifts along the way whilst giving you great satisfaction and pride that you was one of the first holders of this evolutionary ro
+          </p>
+          <h3 className="text-2xl mb-2">GIVING 100 TOKENS TO 100 DIFFERENT NOT FOR PROFITS</h3>
+          <p>
+         We are giving 100 NFT’s to 100 different charity in 10 different industries.
+
+1 to Keith Haring foundation and to 9 organizations of the foundations choice that Keith would want to support.
+1 to NHS Trust and 9 others to healthcare charities for different medical conditions and diseases.
+1 to TeamSeas and 9 others to Climate type organizations.
+10 to mental health education and support.
+10 to helping children access more choices and enjoy some peace of mind.
+10 to helping women who are in danger.
+10 to helping educate and stop discrimination in any form.
+10 to help refugees.
+10 to help human beings access clean water.
+10 to helping animal type charities.
           </p>
         </div>
       </div>
